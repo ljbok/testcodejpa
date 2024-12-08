fb01ec6619c77740f615a880fe5b2b8ac7601827
DESKTOP-UQNJGO7\LJB <dlwnsqhr123@gmail.com>
2024-12-08 17:00:30 +0900

    [add] init commit to add inital projects

---
+++ jpah2/src/main/java/com/testcode/jpah2/Jpah2Application.java
@@ -0,0 +1,13 @@
+package com.testcode.jpah2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpah2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpah2Application.class, args);
+	}
+
+}
---
+++ jpah2/src/test/java/com/testcode/jpah2/Jpah2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcode.jpah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpah2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
